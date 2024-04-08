d306992bc557396667e7b0a5ee5a3bd49b01d044
Eloi <eloi.random@gmail.com>
2024-04-08 13:57:26 +0200

    Amélioration de la clarté du code

--- site josse/script.js
+++ site josse/script.js
@@ -9,16 +9,15 @@ to{
     opacity: 1;
 }
 
-*/ 
+*/
 
 function randint(max) {
     return Math.floor(Math.random() * max);
 }
 
-
 poeme = poemes[randint(poemes.length)];
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
     var poemeElement = document.getElementById("poeme");
     var poemeTitle = document.createElement("h1");
     poemeTitle.id = "title";
@@ -30,32 +29,25 @@ document.addEventListener('DOMContentLoaded', () => {
     poemeElement.appendChild(poemeTitle);
     poemeElement.appendChild(poemeContent);
 
-    var newDom = '';
+    var newDom = "";
     var animationDelay = 50;
 
-
     var words = poemeContent.innerHTML.split(" ");
-    
-    for(let i = 0; i < words.length; i++)
-    {
+
+    for (let i = 0; i < words.length; i++) {
         var w = words[i].split("<br>");
         for (let j = 0; j < w.length; j++) {
-            newDom += '<span class="char">' + (w[j] == ' ' ? '&nbsp;' : w[j] + "&nbsp;")+ '</span>';
+            newDom += '<span class="char">' + (w[j] == " " ? "&nbsp;" : w[j] + "&nbsp;") + "</span>";
             if (w.length > 1 && j != w.length - 1) {
-                newDom += "<br>"
+                newDom += "<br>";
             }
         }
-
     }
 
     poemeContent.innerHTML = newDom;
     var length = poemeContent.children.length;
 
-    for(let i = 0; i < length; i++)
-    {
-        poemeContent.children[i].style['animation-delay'] = animationDelay * i + 'ms';
+    for (let i = 0; i < length; i++) {
+        poemeContent.children[i].style["animation-delay"] = animationDelay * i + "ms";
     }
-
-
 });
-
